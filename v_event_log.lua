5ac25695efab4e00ce6c5ab1b515146cf671271f
pred <97589600+pred14@users.noreply.github.com>
2022-01-12 19:11:51 +0800

    Update v_event_log.lua

--- v_event_log.lua
+++ v_event_log.lua
@@ -1,7 +1,7 @@
 --[[
-    author: pred
+    author: pred#2448
 
-    gamesense like event logs
+    gamesense like event log for neverlose
 ]]
 
 local ffi = require("ffi")
@@ -29,7 +29,7 @@ ffi.cdef[[
     typedef void(__cdecl *ColorMsgFn)(Color&, const char*);
 ]]
 
-local ConsolePrint = function(label, r, g, b, a)
+local colorprint = function(label, r, g, b, a)
     local ConColorMsg = ffi.cast("ColorMsgFn", ffi.C.GetProcAddress(ffi.C.GetModuleHandleA("tier0.dll"), "?ConColorMsg@@YAXABVColor@@PBDZZ"))
     
     local col = ffi.new("Color")
@@ -41,10 +41,15 @@ local ConsolePrint = function(label, r, g, b, a)
     ConColorMsg(col, label)
 end
 
-local log = {
-}
-local console = Render.InitFont("lucida console", 10, {"r"})
-local hitboxes = {
+local SWITCH = Menu.Switch
+local COMBO = Menu.Combo
+local MCOMBO = Menu.MultiCombo
+local SLIDERINT = Menu.SliderInt
+local TEXT = Menu.Text
+local TEXTBOX = Menu.TextBox
+
+local FONT_CONSOLE = Render.InitFont("lucida console", 10, {"r"})
+local HITGROUPS = {
     [0] = "generic",
     [1] = "head",
     [2] = "chest",
@@ -55,88 +60,134 @@ local hitboxes = {
     [7] = "right leg",
     [10] = "gear",
 }
-local reasons = {
-    "resolver",
-    "spread",
-    "occlusion",
-    "prediction error",
+local REASONS = {
+    [0] = "hit",
+    [1] = "resolver",
+    [2] = "spread",
+    [3] = "occlusion",
+    [4] = "prediction error",
 }
 
-local drawlog = function(prefix, prefix_r, prefix_g, prefix_b, prefix_a, text, print_text)
-    log[#log + 1] = {
-        text,
-        255,
-        math.floor(GlobalVars.curtime),
-    }
-    ConsolePrint(prefix, prefix_r, prefix_g, prefix_b, prefix_a)
-    print(print_text)
+local loggerfilter = MCOMBO("LUA", "Event log", {"Hit", "Hurt", "Misses", "Purchase"}, -1)
+local t = {}
+
+local function addlog(string)
+    --insert log function, makes the whole thing easier and cleaner
+    table.insert(t, {
+        text = string,
+        alpha = 255,
+        ctime = GlobalVars.curtime,
+    })
+
+    colorprint("[logger] ", 180, 230, 20, 255)
+    print(string.format("%s \r", string))
 end
 
-Cheat.RegisterCallback("events", function(event)
-    local localplayer = EntityList.GetLocalPlayer()
-    if event:GetName() == "item_purchase" then
-        if event:GetName() ~= "item_purchase" then
-            return
-        end
-        local buyerid = EntityList.GetPlayerForUserID(event:GetInt("userid"))
-        local item = event:GetString("weapon")
-        if buyerid ~= localplayer and item ~= "weapon_unknown" and buyerid ~= nil then
-            local buyer = buyer_ent:GetName()
-            drawlog("[log] ", 180, 230, 20, 255, string.format("%s bought %s", buyer, item), string.format("%s bought %s\r", buyer, item))
-        end
-    end
-    if event:GetName() == "player_hurt" then
-        local target = EntityList.GetPlayerForUserID(event:GetInt("userid"))
-        local attacker = EntityList.GetPlayerForUserID(event:GetInt("attacker"))
-        local target_name = target:GetName()
-        local dmghealth = event:GetInt("dmg_health")
-        local healthremain = event:GetInt("health")
-        local hitbox = hitboxes[event:GetInt("hitgroup")]
-        if attacker == localplayer then
-            drawlog("[log] ", 180, 230, 20, 255, string.format("Hit %s in the %s for %s damage (%s health remaining)", target_name, hitbox, dmghealth, healthremain), string.format("Hit %s in the %s for %s damage (%s health remaining)\r", target_name, hitbox, dmghealth, healthremain))
-        end
-    end
-end)
+local function textoutline(string, x, y, r, g, b, a, fontsize, font, middle)
+    Render.Text(string, Vector2.new(x + 1, y + 1), Color.RGBA(0, 0, 0, a), fontsize, font, false, middle)
+    Render.Text(string, Vector2.new(x, y), Color.RGBA(r, g, b, a), fontsize, font, false, middle)
+end
 
-Cheat.RegisterCallback("registered_shot", function(reg) 
-    local missreason = reasons[reg.reason]
-    if reg.reason == 0 then
+
+local function firedshot(s)
+
+    if not loggerfilter:Get(3) then
         return
     end
-    drawlog("[log] ", 180, 230, 20, 255, string.format("Missed shot due to %s", missreason), string.format("Missed shot due to %s\r", missreason))
+
+    local reason = REASONS[s.reason]
+    local whg = HITGROUPS[s.wanted_hitgroup]
+    local hc = s.hitchance
+    local bt = s.backtrack
+    local wdmg = s.wanted_damage
+    local name = EntityList.GetClientEntity(s.target_index):GetPlayer():GetName()
+    local sprd = s.spread_degree
+
+    if reason == "hit" then
+        return
+    end
+    --miss
+    addlog(string.format("Missed %s's %s(%s) due to %s, spread=%.2f°", name, whg, wdmg, reason, sprd))
+end
+
+Cheat.RegisterCallback("events", function(e)
+    local localplayer = EntityList.GetLocalPlayer()
+    
+    if e:GetName() == "item_purchase" then
+        local buyer_id = EntityList.GetPlayerForUserID(e:GetInt("userid"))
+        local buyer_name = buyer_id:GetName()
+        local item = e:GetString("weapon")
+
+        if buyer_id == localplayer or item == "weapon_unknown" or loggerfilter:Get(4) then
+            return
+        end
+        --purchase
+        addlog(string.format("%s bought %s", buyer_name, item))
+    end
+
+    if e:GetName() == "player_hurt" then
+        local attacker = EntityList.GetPlayerForUserID(e:GetInt("attacker"))
+        local target = EntityList.GetPlayerForUserID(e:GetInt("userid"))
+        local att_name = attacker:GetName()
+        local tar_name = target:GetName()
+        local dmg = e:GetInt("dmg_health")
+        local remain = e:GetInt("health")
+        local hg = HITGROUPS[e:GetInt("hitgroup")]
+
+        if attacker == localplayer and loggerfilter:Get(1) then
+            --hit
+            addlog(string.format("Hit %s in the %s for %i damage (%i health remaining)", tar_name, hg, dmg, remain))
+        end
+
+        if target == localplayer and loggerfilter:Get(2) then
+            --hurt
+            addlog(string.format("Got hit by %s in the %s for %i damage (%i health remaining)", att_name, hg, dmg, remain))
+        end
+    end
 end)
 
-local clearlog = function()
+local function clear()
+    if #t == nil then
+        --if the table has nothing, return.
+        return
+    end
 
-    if #log ~= 0 then
-        if EngineClient:IsConnected() == false then
-            table.remove(log, #log)
-        end 
+    if #t > 6 then
+        --if table have more than 6 indexes, removes first index.
+        table.remove(t, 1)
+    end
 
-        if #log > 6 then
-            table.remove(log, 1)
+    if not EngineClient.IsConnected() then
+        --if client isn't connected, remove all the indexes.
+        table.remove(t, #t)
+        return
+    end
+
+    for i = 1, #t do
+        --if index has nothing, return.
+        if t[i] == nil then
+            return
         end
-        
-        if log[1][3] + 5 < math.floor(GlobalVars.curtime) then
-            log[1][2] = log[1][2] - math.floor(GlobalVars.frametime * 600)
-            if log[1][2] < 0 then
-                table.remove(log, 1)
-            end
+        --timeout
+        if t[i].ctime + 6.5 > math.floor(GlobalVars.curtime) then
+            return
+        end
+        --starts fadeaway
+        t[i].alpha = t[i].alpha - math.floor(GlobalVars.frametime * 300)
+        --if alpha less than 0, removes the index.
+        if t[i].alpha < 0 then
+            table.remove(t, i)
         end
     end
 end
 
-local drawlog = function()
-    clearlog()
-    for i = 1, #log do
-        Render.Text(""..log[i][1].."", Vector2.new(7 + 1, 5 + i * 12 + 1 - 12), Color.RGBA(0, 0, 0, log[i][2]), 10, console)
-        Render.Text(""..log[i][1].."", Vector2.new(7, 5 + i * 12 - 12), Color.RGBA(255, 255, 255, log[i][2]), 10, console)
+local function render()
+    --draw the logs.
+    clear()
+    for i = 1, #t do
+        textoutline(t[i].text, 7, 5 + i * 12 - 12, 255, 255, 255, t[i].alpha, 10, FONT_CONSOLE, false)
     end
 end
 
-Cheat.RegisterCallback("draw", function()
-    if EngineClient.IsConnected() == false then 
-        return 
-    end
-    drawlog()
-end)
+Cheat.RegisterCallback("draw", render)
+Cheat.RegisterCallback("registered_shot", firedshot)
