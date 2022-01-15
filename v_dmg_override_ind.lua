99452d103c88f460fa177a40ff8fb23dbb325a08
pred <97589600+pred14@users.noreply.github.com>
2022-01-15 23:18:53 +0800

    Create v_dmg_override_ind.lua

---
+++ v_dmg_override_ind.lua
@@ -0,0 +1,26 @@
+--[[
+    author: pred#2448
+
+    simple mindmg override indicator [feel free to use it.]
+]]
+
+local function draw()
+
+    local overrided = false
+    local getbinds = Cheat.GetBinds()
+    local sx, sy = EngineClient.GetScreenSize().x, EngineClient.GetScreenSize().y
+    local val
+
+    for i = 1, #getbinds do
+        if getbinds[i]:GetName() == "Minimum Damage" then
+            overrided = true
+            val = tostring(getbinds[i]:GetValue())
+        end
+    end
+
+    if overrided then
+        Render.Text(val, Vector2.new(sx / 2 + 10, sy / 2 - 20), Color.RGBA(255, 255, 255, 255), 10)
+    end
+
+end
+Cheat.RegisterCallback("draw", draw)
